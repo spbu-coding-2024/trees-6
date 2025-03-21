8229003d7a85284ab0f442523f1f784c833e5318
fUS1ONd <koskriv2006@gmail.com>
2025-03-21 21:37:19 +0300

    feat: wrote body of insert() of RBTree

--- src/main/kotlin/trees/redblack/RedBlackNode.kt
+++ src/main/kotlin/trees/redblack/RedBlackNode.kt
@@ -7,8 +7,8 @@ import trees.redblack.Colors
 class RedBlackNode<K: Comparable<K>, V>(
     override val key: K,
     override val value: V,
-    val parent: RedBlackNode<K, V>? = null,
+    var parent: RedBlackNode<K, V>? = null,
     override var left: RedBlackNode<K, V>? = null,
     override var right: RedBlackNode<K, V>? = null,
-    val color: Colors = Colors.RED
+    var color: Colors = Colors.RED
 ): Node<K, V, RedBlackNode<K, V>>
--- src/main/kotlin/trees/redblack/RedBlackTree.kt
+++ src/main/kotlin/trees/redblack/RedBlackTree.kt
@@ -1,7 +1,57 @@
 package trees.redblack
 
 import trees.base.AbstractTree
-import trees.base.Tree
 
-class RedBlackTree {
+class RedBlackTree<K: Comparable<K>, V> : AbstractTree<K, V, RedBlackNode<K, V>>() {
+    private var root: RedBlackNode<K, V>? = null
+    private var countNodes: Int = 0
+
+    override fun getRoot(): RedBlackNode<K, V>>? { return root }
+    override fun setRoot(newValue: RedBlackNode<K, V>>?): Boolean { root = newValue; return true }
+
+    override fun insert(key: K, value: V): RedBlackNode<K, V> {
+        val newNode: RedBlackNode<K, V> = RedBlackNode(key, value)
+
+
+        if (isEmpty()) {
+            setRoot(newNode)
+            newNode.color = Colors.BLACK
+            addOneToCountNodes()
+        } else {
+            var parent: RedBlackNode<K, V>? = getRoot()
+            var current: RedBlackNode<K, V>? = getRoot()
+
+            while (current != null) {
+                parent = current
+                current = when {
+                    key > current.key -> current.right
+                    else -> current.left
+                }
+            }
+
+            newNode.parent = parent
+            
+            when {
+                parent == null -> setRoot(newNode)
+                key > parent.key -> parent.right = newNode
+                else -> parent.left = newNode
+            }
+
+            addOneToCountNodes()
+            fixInsertion(newNode) // balance RBTree
+        }
+        return newNode
+    }
+
+    private fun fixInsertion(node: RedBlackNode<K, V>) {}
+
+    override fun delete(value: K): Boolean {
+        TODO("Not yet implemented")
+    }
+    override fun search(key: K): V? {
+        TODO("Not yet implemented")
+    }
+    override fun height(): Int {
+        TODO("Not yet implemented")
+    }
 }
