13ea8cda67b7dd385c11a7d64c97f18d94099e71
Maksim Varlyga <maksvarlyga@gmail.com>
2025-03-22 01:25:04 +0300

    fix(AVLNode): added small changes

--- src/main/kotlin/trees/avl/AVLNode.kt
+++ src/main/kotlin/trees/avl/AVLNode.kt
@@ -2,10 +2,10 @@ package trees.avl
 
 import trees.base.Node
 
-class AVLNode<K: Comparable<K>, V>(
-    override val key: K,
-    override val value: V,
+class AVLNode<K : Comparable<K>, V>(
+    override var key: K,
+    override var value: V,
     override var left: AVLNode<K, V>? = null,
     override var right: AVLNode<K, V>? = null,
     var height: Int = 1
-): Node<K, V, AVLNode<K, V>>
+) : Node<K, V, AVLNode<K, V>>
