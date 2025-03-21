874ff4a60e6e2fe2651fa05dd5bc3754146735df
fUS1ONd <koskriv2006@gmail.com>
2025-03-21 22:10:03 +0300

    feat: wrote balance fun according to wiki

--- src/main/kotlin/trees/redblack/RedBlackTree.kt
+++ src/main/kotlin/trees/redblack/RedBlackTree.kt
@@ -6,12 +6,8 @@ class RedBlackTree<K: Comparable<K>, V> : AbstractTree<K, V, RedBlackNode<K, V>>
     private var root: RedBlackNode<K, V>? = null
     private var countNodes: Int = 0
 
-    override fun getRoot(): RedBlackNode<K, V>>? { return root }
-    override fun setRoot(newValue: RedBlackNode<K, V>>?): Boolean { root = newValue; return true }
-
     override fun insert(key: K, value: V): RedBlackNode<K, V> {
         val newNode: RedBlackNode<K, V> = RedBlackNode(key, value)
-
 
         if (isEmpty()) {
             setRoot(newNode)
@@ -38,19 +34,72 @@ class RedBlackTree<K: Comparable<K>, V> : AbstractTree<K, V, RedBlackNode<K, V>>
             }
 
             addOneToCountNodes()
-            fixInsertion(newNode) // balance RBTree
+            balance(newNode) // balance RBTree
         }
         return newNode
     }
 
-    private fun fixInsertion(node: RedBlackNode<K, V>) {}
+    private fun balance(node: RedBlackNode<K, V>) {
+        var current: RedBlackNode<K, V> = node
+        if (current == getRoot()) {
+            current.color = Colors.BLACK
+            return
+        }
+
+        while (current.parent?.color == Colors.RED) {
+            val parent = current.parent
+            val grandParent = parent?.parent
+
+            if (parent == grandParent?.left) {
+                val uncle = grandParent?.right
+
+                if (uncle?.color == Colors.RED) {
+                    parent.color = Colors.BLACK
+                    uncle.color = Colors.BLACK
+                    grandParent.color = Colors.RED
+                    current = grandParent
+                } else {
+                    if (current == parent?.right) {
+                        current = parent
+                        rotateLeft(current)
+                    }
+                    parent?.color = Colors.BLACK
+                    grandParent?.color = Colors.RED
+                    rotateRight(grandParent)
+                }
+            } else {
+                val uncle = grandParent?.left
+
+                if (uncle?.color == Colors.RED) {
+                    parent.color = Colors.BLACK
+                    uncle.color = Colors.BLACK
+                    grandParent.color = Colors.RED
+                    current = grandParent
+                } else {
+                    if (current == parent?.left) {
+                        current = parent
+                        rotateRight(current)
+                    }
+                    parent?.color = Colors.BLACK
+                    grandParent?.color = Colors.RED
+                    rotateLeft(grandParent)
+                }
+            }
+        }
+    }
+
+    private fun rotateLeft(node: RedBlackNode<K, V>?) {}
+
+    private fun rotateRight(node: RedBlackNode<K, V>?) {}
 
     override fun delete(value: K): Boolean {
         TODO("Not yet implemented")
     }
+
     override fun search(key: K): V? {
         TODO("Not yet implemented")
     }
+
     override fun height(): Int {
         TODO("Not yet implemented")
     }
