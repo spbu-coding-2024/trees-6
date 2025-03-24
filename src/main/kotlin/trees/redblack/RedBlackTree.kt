03082ca8478c372bc8eccc7427babb0367aae963
fUS1ONd <koskriv2006@gmail.com>
2025-03-24 20:26:51 +0300

    feat: rewrite insertion RBTree's

--- src/main/kotlin/trees/redblack/RedBlackTree.kt
+++ src/main/kotlin/trees/redblack/RedBlackTree.kt
@@ -7,17 +7,93 @@ class RedBlackTree<K: Comparable<K>, V> : AbstractTree<K, V, RedBlackNode<K, V>>
     private var countNodes: Int = 0
 
     override fun insert(key: K, value: V): RedBlackNode<K, V> {
-        val newNode: RedBlackNode<K, V> = RedBlackNode(key, value)
+        var newNode: RedBlackNode<K, V> = RedBlackNode(key, value)
+
+        var parentFinder: RedBlackNode<K, V>? = null
+        var placeOfNewNode: RedBlackNode<K, V>? = getRoot()
+        while (placeOfNewNode != null) {
+            parentFinder = placeOfNewNode
+            if (newNode.key < placeOfNewNode.key) {
+                placeOfNewNode = placeOfNewNode.left
+            } else {
+                placeOfNewNode = placeOfNewNode.right
+            }
+        }
+
+        newNode.parent = parentFinder
+
+        if (parentFinder == null) {
+            setRoot(newNode)
+        } else if (newNode.key < parentFinder.key) {
+            parentFinder.left = newNode
+        } else {
+            parentFinder.right = newNode
+        }
+
+        fixInsertion(newNode)
+
+        return newNode
     }
 
-    private fun balance(node: RedBlackNode<K, V>) {
+    private fun fixInsertion(originNode: RedBlackNode<K, V>) {
+        // Тк в котлине в фукнцию передаются параметры, доступные
+        // только для чтения, вынужден создать копию передаваемой ноды
+        var node: RedBlackNode<K, V>? = originNode
 
+        while (node?.parent?.color == Colors.RED) {
+            if (node.parent == node.parent?.parent?.left) {
+                var uncleOfNode: RedBlackNode<K, V>? = node.parent?.parent?.right
+
+                if (uncleOfNode?.color == Colors.RED) {
+                    node.parent?.color = Colors.BLACK
+                    uncleOfNode.color = Colors.BLACK
+                    node.parent?.parent?.color = Colors.RED
+                    node = node.parent?.parent
+                } else {
+                    if (node == node.parent?.right) {
+                        node = node.parent
+                        rotateLeft(node)
+                    }
+
+                    node?.parent?.color = Colors.BLACK
+                    node?.parent?.parent?.color = Colors.RED
+                    rotateRight(node?.parent?.parent)
+                }
+            } else {
+                var uncleOfNode: RedBlackNode<K, V>? = node.parent?.parent?.left
+
+                if (uncleOfNode?.color == Colors.RED) {
+                    node.parent?.color = Colors.BLACK
+                    uncleOfNode.color = Colors.BLACK
+                    node.parent?.parent?.color = Colors.RED
+                    node = node.parent?.parent
+                } else {
+                    if (node == node.parent?.left) {
+                        node = node.parent
+                        rotateRight(node)
+                    }
+
+                    node?.parent?.color = Colors.BLACK
+                    node?.parent?.parent?.color = Colors.RED
+                    rotateLeft(node?.parent?.parent)
+                }
+            }
+
+            if (node == getRoot()) {
+                break
+            }
+        }
+
+        // Root дерева запривачен, поэтому копипаст и setRoot
+        val fixRoot: RedBlackNode<K, V>? = getRoot()
+        fixRoot?.color = Colors.BLACK
+        setRoot(fixRoot)
     }
 
-    private fun rotateLeft(target: RedBlackNode<K, V>) {
+    private fun rotateLeft(target: RedBlackNode<K, V>?) {
         
-        var copyOfRightSon: RedBlackNode<K, V>? = target.right
-        target.right = copyOfRightSon?.left
+        val copyOfRightSon: RedBlackNode<K, V>? = target?.right
+        target?.right = copyOfRightSon?.left
         
         // устанавливаем relationships
         if (copyOfRightSon?.left != null) {
@@ -25,9 +101,9 @@ class RedBlackTree<K: Comparable<K>, V> : AbstractTree<K, V, RedBlackNode<K, V>>
         }
 
         // Отцом копии назначаем деда
-        copyOfRightSon?.parent = target.parent
+        copyOfRightSon?.parent = target?.parent
 
-        if (target.parent == null) {
+        if (target?.parent == null) {
             setRoot(copyOfRightSon)
         } else if (target == target.parent?.left) {
             target.parent?.left = copyOfRightSon
@@ -36,30 +112,30 @@ class RedBlackTree<K: Comparable<K>, V> : AbstractTree<K, V, RedBlackNode<K, V>>
         }
 
         copyOfRightSon?.left = target
-        target.parent = copyOfRightSon
+        target?.parent = copyOfRightSon
     }
 
-    private fun rotateRight(target: RedBlackNode<K, V>) {
+    private fun rotateRight(target: RedBlackNode<K, V>?) {
 
-        var copyOfLeftSon: RedBlackNode<K, V>? = target.left
-        target.left = copyOfLeftSon?.right
+        val copyOfLeftSon: RedBlackNode<K, V>? = target?.left
+        target?.left = copyOfLeftSon?.right
 
         if (copyOfLeftSon?.right != null) {
             copyOfLeftSon.right?.parent = target
         }
 
-        copyOfLeftSon?.parent = target.parent
+        copyOfLeftSon?.parent = target?.parent
 
-        if (target.parent == null) {
+        if (target?.parent == null) {
             setRoot(copyOfLeftSon)
-        } else if (target == target.parent?.left) {
-            target.parent?.left = copyOfLeftSon
-        } else {
+        } else if (target == target.parent?.right) {
             target.parent?.right = copyOfLeftSon
+        } else {
+            target.parent?.left = copyOfLeftSon
         }
 
         copyOfLeftSon?.right = target
-        target.parent = copyOfLeftSon
+        target?.parent = copyOfLeftSon
     }
 
     override fun delete(key: K): Boolean {
