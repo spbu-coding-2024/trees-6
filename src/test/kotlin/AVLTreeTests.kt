075fb9b2842b3d4838df8969328d02bfe6e2d2fb
Maksim Varlyga <maksvarlyga@gmail.com>
2025-03-26 17:14:08 +0300

    feat(AVLTreeTests): added new tests for AVLTree

--- src/test/kotlin/AVLTreeTests.kt
+++ src/test/kotlin/AVLTreeTests.kt
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.apache.commons.lang3.RandomStringUtils
+import trees.avl.AVLNode
 import trees.avl.AVLTree
 import kotlin.math.pow
 import kotlin.random.Random
 
 class AVLTreeTests {
+    private val stringLength = 20
+    private fun randomStringByApacheCommons() = RandomStringUtils.randomAlphanumeric(stringLength)
 
     @Test
     fun `Basic AVL test with simply insert and search and delete`() {
@@ -39,7 +43,7 @@ class AVLTreeTests {
     }
 
     @Test
-    fun `Complicated AVL test with varying height`() {
+    fun `Complicated AVL random test with varying height`() {
         val avl = AVLTree<Int, String>()
         val minHeight = 1
         val maxExponent = 12.0
@@ -67,7 +71,27 @@ class AVLTreeTests {
     }
 
     @Test
-    fun `Basic AVL test with empty tree and isEmpty() function`() {
+    fun `Basic AVL test with clear() method`() {
+        val avl = AVLTree<Int, Int>()
+        avl.insert(10, 10)
+        avl.clear()
+        assertEquals(0, avl.height())
+    }
+
+    @Test
+    fun `Basic AVL test with size() method`() {
+        val avl = AVLTree<Int, Int>()
+        val cntElements = 5
+        for (i in 1..cntElements) {
+            avl.insert(i, i)
+        }
+        assertEquals(cntElements, avl.size())
+        avl.clear()
+        assertEquals(0, avl.size())
+    }
+
+    @Test
+    fun `Basic AVL test with empty tree and isEmpty() method`() {
         val avl = AVLTree<Int, String>()
         assertTrue(avl.isEmpty())
     }
@@ -76,6 +100,12 @@ class AVLTreeTests {
     fun `Basic AVL test with empty tree and height() method`() {
         val avl = AVLTree<Int, String>()
         assertEquals(0, avl.height())
+    }
+
+    @Test
+    fun `Basic AVL test with empty tree and delete() method`() {
+        val avl = AVLTree<Int, String>()
+        assertFalse(avl.delete(10))
     }
 
     @Test
@@ -92,5 +122,68 @@ class AVLTreeTests {
             assertNull(avl.search(i))
         assertEquals(0, avl.height())
         assertTrue(avl.isEmpty())
+    }
+
+    @Test
+    fun `Complicated AVL random test inOrder() method`() {
+        val avl = AVLTree<Int, Int>()
+        val allNumbers = mutableListOf<Int>()
+        val cntNumbers = Random.nextInt(1, 3000)
+
+        for (i in 1..cntNumbers) {
+            val currentInt = Random.nextInt()
+            avl.insert(currentInt, currentInt)
+            allNumbers.add(currentInt)
+        }
+
+        val order = avl.inOrder()
+        allNumbers.sort()
+
+        for (i in 0..<cntNumbers) {
+            val currentInt = allNumbers[i]
+            val currentIntInNode = order[i].key
+            assertEquals(currentInt, currentIntInNode)
+        }
+    }
+
+    @Test
+    fun `Complicated AVL random test with 3000 nodes and strings as keys and int as values`() {
+        val avl = AVLTree<String, Int>()
+        val cntElements = 3000
+        val allNodes = mutableListOf<AVLNode<String, Int>>()
+
+        // Filling in the data
+        for (i in 1..cntElements) {
+            val key = randomStringByApacheCommons()
+            val value = Random.nextInt()
+            val newNode = AVLNode<String, Int>(key, value)
+            allNodes.add(newNode)
+            avl.insert(key, value)
+        }
+
+        // Checking tree
+        for (i in 0..<cntElements) {
+            val currentKey = allNodes[i].key
+            val currentValue = allNodes[i].value
+            assertEquals(avl.search(currentKey), currentValue)
+        }
+
+        // Delete some nodes from tree
+        val cntDelete = Random.nextInt(1, cntElements)
+        val allDeleted = mutableListOf<AVLNode<String, Int>>()
+        for (i in 0..<cntDelete) {
+            val deleteNode = AVLNode<String, Int>(allNodes[i].key, allNodes[i].value)
+            allDeleted.add(deleteNode)
+            val deleteStatus = avl.delete(deleteNode.key)
+            assertTrue(deleteStatus)
+        }
+        for (i in 0..<cntDelete) {
+            allNodes.remove(allDeleted[i])
+        }
+
+        // Checking tree
+        for (i in 0..<cntDelete) {
+            assertNull(avl.search(allDeleted[i].key))
+        }
     }
 }
