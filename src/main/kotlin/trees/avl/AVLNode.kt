6a66a83adbb58d8b74a811d417f5cf902b32324c
Maksim Varlyga <maksvarlyga@gmail.com>
2025-03-30 23:03:08 +0300

    chore(AVLNode): formatting the code using ktlint

--- src/main/kotlin/trees/avl/AVLNode.kt
+++ src/main/kotlin/trees/avl/AVLNode.kt
@@ -20,5 +20,5 @@ class AVLNode<K : Comparable<K>, V>(
     override var value: V,
     override var left: AVLNode<K, V>? = null,
     override var right: AVLNode<K, V>? = null,
-    var height: Int = 1
+    var height: Int = 1,
 ) : Node<K, V, AVLNode<K, V>>
