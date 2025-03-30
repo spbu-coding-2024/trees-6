a68f1780f29158e74b98117ec0cf3595796c2544
Maksim Varlyga <maksvarlyga@gmail.com>
2025-03-30 23:06:56 +0300

    chore(Tree): formatting the code using ktlint

--- src/main/kotlin/trees/base/Tree.kt
+++ src/main/kotlin/trees/base/Tree.kt
@@ -2,13 +2,20 @@ package trees.base
 
 interface Tree<K : Comparable<K>, V, N : Node<K, V, N>> {
     // methods for changing the tree
-    fun insert(key: K, value: V): N
+    fun insert(
+        key: K,
+        value: V,
+    ): N
+
     fun delete(key: K): Boolean
+
     fun search(key: K): V?
+
     fun clear()
 
     // methods for getting values
     fun size(): Int
+
     fun inOrder(): List<Node<K, V, N>>
 
     // methods for checks
